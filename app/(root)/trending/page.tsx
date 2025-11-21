1fd3270c0ff86bc6e120bd5987505de06e773d9c
Thanh Nguyen <thnhnguyn761@gmail.com>
2025-11-21 16:42:19 +0700

    Trending

---
+++ app/(root)/trending/page.tsx
@@ -0,0 +1,80 @@
+import BookItem from '@/components/BookItem'
+import React from 'react'
+
+const TrendingPage = () => {
+  return (
+    <div className="m-5">
+      <div>
+        <div className="text-[28px]/[28px] font-medium text-white">Xu hướng trong tuần</div>
+        <div className="flex mb-8 justify-between">
+          <div className="text-[40px]/[50px] font-semibold text-white mt-2.5">Sách hay trong tuần
+          </div>
+        </div>
+        <div className="mt-0 p-0! flex flex-wrap -mx-[15px]">
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+          <div className="pb-6! flex-none w-1/4 max-w-[25%] relative min-h-px px-[15px]">
+            <BookItem />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TrendingPage
