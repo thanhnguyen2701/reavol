0022bc4c96ba4bb120e8326389361d5a17acd132
Thanh Nguyen <thnhnguyn761@gmail.com>
2025-11-25 11:57:31 +0700

    Bookfree Page Responsive

--- app/(root)/bookfree/page.tsx
+++ app/(root)/bookfree/page.tsx
@@ -12,16 +12,20 @@ const BookFreePage = async () => {
         <div className="m-5">
             <div className="mt-2.5">
                 <div className="text-[30px] font-semibold text-white leading-7">SÁCH MIỄN PHÍ</div>
-                <div className="relative aspect-[2.96] w-full mt-8 mb-20">
+                <div className="relative aspect-square md:aspect-[2.96] w-full mt-8 mb-20">
                     <span className='box-border block overflow-hidden w-[initial] h-[initial] opacity-100 m-0 p-0 absolute inset-0 rounded-[20px]'>
-                        <Image src='/new_mediumiiH.jpg' width={1496} height={505} alt='' className="absolute inset-0 box-border p-0 m-auto block bg-cover bg-center" />
+                        {
+                            freeList ? 
+                            <Image src={freeList[0].media.originUrl} width={1496} height={505} alt='' className="absolute inset-0 box-border p-0 m-auto block bg-cover bg-center" />
+                            : <></>
+                        }
                     </span>
                 </div>
             </div>
             <div className="flex flex-wrap -mx-[15px]">
                 {
                     freeList?.map((item) => (
-                        <div key={item.id} className="basis-[50%] max-w-[50%] relative w-full min-h-px px-[15px]">
+                        <div key={item.id} className="basis-full md:basis-[50%] max-w-full md:max-w-[50%] relative w-full min-h-px px-[15px]">
                             <BookItemComponent2 item={item} />
                         </div>
                     ))
